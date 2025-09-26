51a8d29ab655c5ced1c5e865770f220019a8b493
webdmitriev <webdmitriev@gmail.com>
2025-09-26 14:20:18 +0300

    block-02 gutenberg is ready

--- webdmitriev/blocks/block-02.php
+++ webdmitriev/blocks/block-02.php
@@ -10,17 +10,27 @@ $url = get_template_directory_uri();
 $image_base64 = 'data:image/gif;base64,R0lGODlhBwAFAIAAAP///wAAACH5BAEAAAEALAAAAAAHAAUAAAIFjI+puwUAOw==';
 
 $allowed_tags = array(
-  'br'  => array()
+  'br'    => array(),
+  'span'  => array(
+    'class' => array(),
+  )
 );
 
-$text     = wp_kses(get_field('text'), $allowed_tags);
-$link     = esc_url(get_field('link'));
-$bg_1920  = get_field('bg_1920') ? "background-image: url(" . esc_url(get_field('bg_1920')) . ")"  : false;
+$block_id   = wp_kses(get_field('block_id'), $allowed_tags);
+$block_bgc  = get_field('block_bgc') ? 'background-color:' . get_field('block_bgc') : false;
+
+$title              = wp_kses(get_field('title'), $allowed_tags);
+$sub_title          = wp_kses(get_field('sub_title'), $allowed_tags);
+$sub_title_2        = wp_kses(get_field('sub_title_2'), $allowed_tags);
+$blockquote_title   = wp_kses(get_field('blockquote_title'), $allowed_tags);
+$blockquote_descr   = wp_kses(get_field('blockquote_descr'), $allowed_tags);
+$blockquote_image   = esc_url(get_field('blockquote_image'));
+$elements           = get_field('elements'); // title, image, descr
 
 ?>
 
 <!-- <?= $block_path; ?> (start) -->
-<section class="block-02">
+<section class="block-02" id="<?= $block_id; ?>" style="<?= $block_bgc; ?>">
   <?php if( is_admin() ) : ?>
     <style>[data="gutenberg-preview-img"] img {width: 100%;object-fit: contain;}</style>
     <div class="gutenberg-block" style="padding: 10px 20px;background-color: #F5F5F5;border: 1px solid #D1D1D1;"><?= $gutenberg_title; ?></div>
@@ -30,60 +40,29 @@ $bg_1920  = get_field('bg_1920') ? "background-image: url(" . esc_url(get_field(
   <?php if( !is_admin() ) : ?>
     <div class="container">
       <div class="line-wrap">
-        <h2 class="h2">Что входит в<span class="accent-color">&nbsp;наши услуги</span> <br/>по&nbsp;шлифовке паркета?</h2>
-        <p class="descr">Мы&nbsp;предлагаем полный комплекс профессиональных паркетных работ. Наши мастера не&nbsp;только шлифуют и&nbsp;циклюют полы, но&nbsp;и&nbsp;проводят дополнительные работы по&nbsp;уходу, защите и&nbsp;декору деревянных покрытий.</p>
-        <p class="descr uppercase bold accent-color">&nbsp;Перечень следующий:</p>
+        <?php if($title): ?><h2 class="h2"><?= $title; ?></h2><?php endif; ?>
+        <?php if($sub_title): ?><p class="descr"><?= $sub_title; ?></p><?php endif; ?>
+        <?php if($sub_title_2): ?><p class="descr uppercase bold accent-color"><?= $sub_title_2; ?></p><?php endif; ?>
 
         <div class="blockquote">
-          <div class="blockquote__label">Такелаж мебели</div>
-          <div class="blockquote__descr">Аккуратно передвинем или вынесем мебель с&nbsp;помощью профессиональных инструментов</div>
-          <picture>
-            <source srcset="<?= $image_base64; ?>" type="image/jpeg" media="(max-width: 768px)">
-            <img class="blockquote__img" src="<?php echo esc_url($url); ?>/webdmitriev/assets/img/block-02/instruments-01.png" alt="Image" />
-          </picture>
+          <?php if($blockquote_title): ?><div class="blockquote__label"><?= $blockquote_title; ?></div><?php endif; ?>
+          <?php if($blockquote_descr): ?><div class="blockquote__descr"><?= $blockquote_descr; ?></div><?php endif; ?>
+          <?php if($blockquote_image): ?>
+            <picture>
+              <source srcset="<?= $image_base64; ?>" type="image/jpeg" media="(max-width: 768px)">
+              <img class="blockquote__img" src="<?= $blockquote_image; ?>" alt="Image" />
+            </picture>
+          <?php endif; ?>
         </div>
 
         <div class="block__items">
-          <div class="block__item">
-            <img class="block__item-img" src="<?php echo esc_url($url); ?>/webdmitriev/assets/img/block-02/post-01.jpg" alt="Image" />
-            <h4 class="block__item-label">Шлифовка паркета <br/>без пыли</h4>
-            <p class="descr">Высококачественная шлифовка полов, в&nbsp;том числе еврошлифовка с&nbsp;применением немецкого оборудования Lagler</p>
-          </div>
-          <div class="block__item">
-            <img class="block__item-img" src="<?php echo esc_url($url); ?>/webdmitriev/assets/img/block-02/post-02.jpg" alt="Image" />
-            <h4 class="block__item-label">Заделка щелей <br/>и&nbsp;микротрещин</h4>
-            <p class="descr">В&nbsp;совершенстве владеем всеми видами шпалкевочных составом. Сделаем пол прочным, ровным и&nbsp;экологичным.</p>
-          </div>
-          <div class="block__item">
-            <img class="block__item-img" src="<?php echo esc_url($url); ?>/webdmitriev/assets/img/block-02/post-03.jpg" alt="Image" />
-            <h4 class="block__item-label">Замена поврежденных элементов</h4>
-            <p class="descr">Подберем по&nbsp;цвету, оттенку паркетную плашку и&nbsp;поставим ее&nbsp;на&nbsp;место поврежденной (недостающей).</p>
-          </div>
-          <div class="block__item">
-            <img class="block__item-img" src="<?php echo esc_url($url); ?>/webdmitriev/assets/img/block-02/post-04.jpg" alt="Image" />
-            <h4 class="block__item-label">Тонировка паркета</h4>
-            <p class="descr">Обработка тонирующими составами для смены оттенка паркета и&nbsp;обновления интерьера</p>
-          </div>
-          <div class="block__item">
-            <img class="block__item-img" src="<?php echo esc_url($url); ?>/webdmitriev/assets/img/block-02/post-05.jpg" alt="Image" />
-            <h4 class="block__item-label">Полировка лака после нанесения</h4>
-            <p class="descr">Сделаем Ваш пол идеально гладким с&nbsp;эффектом зеркального блеска</p>
-          </div>
-          <div class="block__item">
-            <img class="block__item-img" src="<?php echo esc_url($url); ?>/webdmitriev/assets/img/block-02/post-06.jpg" alt="Image" />
-            <h4 class="block__item-label">Грубая циклевка</h4>
-            <p class="descr">Восстановим сильно поврежденный, старый паркет, даже с&nbsp;большими перепадами с&nbsp;помощью барабанных и&nbsp;ленточных машин</p>
-          </div>
-          <div class="block__item">
-            <img class="block__item-img" src="<?php echo esc_url($url); ?>/webdmitriev/assets/img/block-02/post-07.jpg" alt="Image" />
-            <h4 class="block__item-label">Нанесение защитных составов</h4>
-            <p class="descr">Для повышенной износостойкости покрываем лаком. Для ценителей натуральности и&nbsp;приятных тактильных ощущений под ногами рекомендуем выбрать масло</p>
-          </div>
-          <div class="block__item">
-            <img class="block__item-img" src="<?php echo esc_url($url); ?>/webdmitriev/assets/img/block-02/post-08.jpg" alt="Image" />
-            <h4 class="block__item-label">Локальный ремонт</h4>
-            <p class="descr">Восстановим практически любые повреждения, удаляем царапины, пятна, ожоги и&nbsp;прочие дефекты</p>
-          </div>
+          <?php if( have_rows('elements') ) : while ( have_rows('elements') ) : the_row(); ?>
+            <div class="block__item">
+              <img class="block__item-img" src="<?= get_sub_field('image') ? get_sub_field('image') : $image_base64; ?>" alt="Image" />
+              <?php if(get_sub_field('title')): ?><h4 class="block__item-label"><?= get_sub_field('title'); ?></h4><?php endif; ?>
+              <?php if(get_sub_field('descr')): ?><p class="descr"><?= get_sub_field('descr'); ?></p><?php endif; ?>
+            </div>
+          <?php endwhile; endif; ?>
         </div>
 
       </div>
