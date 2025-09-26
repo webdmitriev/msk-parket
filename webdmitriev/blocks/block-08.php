2876531f9a19017794c9f1776f16747007bf9ee5
webdmitriev <webdmitriev@gmail.com>
2025-09-26 17:11:25 +0300

    block-08 gutenberg is ready

--- webdmitriev/blocks/block-08.php
+++ webdmitriev/blocks/block-08.php
@@ -10,18 +10,24 @@ $url = get_template_directory_uri();
 $image_base64 = 'data:image/gif;base64,R0lGODlhBwAFAIAAAP///wAAACH5BAEAAAEALAAAAAAHAAUAAAIFjI+puwUAOw==';
 
 $allowed_tags = array(
-  'br'  => array()
+  'br'    => array(),
+  'span'  => array(
+    'class' => array(),
+  )
 );
 
-$text     = wp_kses(get_field('text'), $allowed_tags);
-$link     = esc_url(get_field('link'));
-$bg_1920  = get_field('bg_1920') ? "background-image: url(" . esc_url(get_field('bg_1920')) . ")"  : false;
-$cf7      = '[contact-form-7 id="99870d1" title="block-08"]';
+$block_id = wp_kses(get_field('block_id'), $allowed_tags);
+$block_bgc      = get_field('block_bgc') ? 'background-color:' . get_field('block_bgc') : false;
+
+$title      = wp_kses(get_field('title'), $allowed_tags);
+$elements   = get_field('elements'); // title, image, descr
+$title_form = wp_kses(get_field('title_form'), $allowed_tags);
+$cf7        = get_field('cf7');
 
 ?>
 
 <!-- <?= $block_path; ?> (start) -->
-<section class="block-08">
+<section class="block-08" id="<?= $block_id; ?>" style="<?= $block_bgc; ?>">
   <?php if( is_admin() ) : ?>
     <style>[data="gutenberg-preview-img"] img {width: 100%;object-fit: contain;}</style>
     <div class="gutenberg-block" style="padding: 10px 20px;background-color: #F5F5F5;border: 1px solid #D1D1D1;"><?= $gutenberg_title; ?></div>
@@ -30,43 +36,29 @@ $cf7      = '[contact-form-7 id="99870d1" title="block-08"]';
 
   <?php if( !is_admin() ) : ?>
     <div class="container">
-      <h2 class="h2">Наши шлифовальные машины</h2>
-      <div class="grinding-arrows">
-        <button class="grinding-arrow grinding-arrow-prev"><svg width="11" height="22" viewBox="0 0 11 22" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.70451 21.5L9.29541 11L0.70451 0.5" stroke="#28211F" stroke-width="1.5"/></svg></button>
-        <button class="grinding-arrow grinding-arrow-next"><svg width="11" height="22" viewBox="0 0 11 22" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.70451 21.5L9.29541 11L0.70451 0.5" stroke="#28211F" stroke-width="1.5"/></svg></button>
-      </div>
-      <div class="grinding__slider">
-        <div class="grinding__slide">
-          <img class="grinding__slide-image" src="<?php echo esc_url($url); ?>/webdmitriev/assets/img/block-08/grinding-01.png" alt="Image" />
-          <div class="grinding__slide-content">
-            <p class="descr bold accent-color">Lagler Trio</p>
-            <p class="descr">Идеальна для тонкой и&nbsp;финишной шлифовки, придает поверхности абсолютную гладкость.</p>
-          </div>
+      <?php if($title): ?><h2 class="h2"><?= $title; ?></h2><?php endif; ?>
+
+      <?php if( have_rows('elements') ) : ?>
+        <div class="grinding-arrows">
+          <button class="grinding-arrow grinding-arrow-prev"><svg width="11" height="22" viewBox="0 0 11 22" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.70451 21.5L9.29541 11L0.70451 0.5" stroke="#28211F" stroke-width="1.5"/></svg></button>
+          <button class="grinding-arrow grinding-arrow-next"><svg width="11" height="22" viewBox="0 0 11 22" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.70451 21.5L9.29541 11L0.70451 0.5" stroke="#28211F" stroke-width="1.5"/></svg></button>
         </div>
-        <div class="grinding__slide">
-          <img class="grinding__slide-image" src="<?php echo esc_url($url); ?>/webdmitriev/assets/img/block-08/grinding-02.png" alt="Image" />
-          <div class="grinding__slide-content">
-            <p class="descr bold accent-color">Lagler Hummel</p>
-            <p class="descr">Основная машина для грубой шлифовки и&nbsp;циклевки; устраняет перепады и&nbsp;глубокие повреждения.</p>
-          </div>
+
+        <div class="grinding__slider">
+          <?php while ( have_rows('elements') ) : the_row(); ?>
+            <div class="grinding__slide">
+              <img class="grinding__slide-image" src="<?= get_sub_field('image'); ?>" alt="Image" />
+              <div class="grinding__slide-content">
+                <p class="descr bold accent-color"><?= get_sub_field('title'); ?></p>
+                <p class="descr"><?= get_sub_field('descr'); ?></p>
+              </div>
+            </div>
+          <?php endwhile; ?>
         </div>
-        <div class="grinding__slide">
-          <img class="grinding__slide-image" src="<?php echo esc_url($url); ?>/webdmitriev/assets/img/block-08/grinding-03.png" alt="Image" />
-          <div class="grinding__slide-content">
-            <p class="descr bold accent-color">Lagler Flip</p>
-            <p class="descr">Незаменима для работы в&nbsp;углах, возле плинтусов и&nbsp;в&nbsp;труднодоступных зонах.</p>
-          </div>
-        </div>
-        <div class="grinding__slide">
-          <img class="grinding__slide-image" src="<?php echo esc_url($url); ?>/webdmitriev/assets/img/block-08/grinding-02.png" alt="Image" />
-          <div class="grinding__slide-content">
-            <p class="descr bold accent-color">Lagler Hummel</p>
-            <p class="descr">Основная машина для грубой шлифовки и&nbsp;циклевки; устраняет перепады и&nbsp;глубокие повреждения.</p>
-          </div>
-        </div>
-      </div>
+      <?php endif; ?>
+
       <div class="block__bottom">
-        <h4 class="block__label">Технология не&nbsp;прощает ошибок&nbsp;&mdash; выбирайте тех, кто работает с&nbsp;лучшими инструментами. Оформите заявку онлайн!</h4>
+        <?php if($title_form): ?><h4 class="block__label"><?= $title_form; ?></h4><?php endif; ?>
         <?= $cf7 ?? do_shortcode($cf7); ?>
       </div>
     </div>
